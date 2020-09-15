aff63d2c24b9a43de27a6c32fbe8cdfd3fb3c37b
tmoran18 <moran.tim01@gmail.com>
2020-09-15 19:51:06 +1000

    Added copy to clipboard function on LinkCard component

--- src/components/LinkCard/LinkCard.js
+++ src/components/LinkCard/LinkCard.js
@@ -1,16 +1,34 @@
-import React from "react";
-import styles from "./LinkCard.module.css";
+import React, { useState } from 'react';
+import styles from './LinkCard.module.css';
 
 const LinkCard = ({ url, hashid }) => {
-  return (
-    <div id={styles.linkCard_container}>
-      <div>{url}</div>
-      <div>
-        <span className={styles.link_url}>{`https://rel.ink/${hashid}`}</span>
-        <button>Copy</button>
-      </div>
-    </div>
-  );
+	const [copySuccess, setCopySuccess] = useState('Copy');
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(`https://rel.ink/${hashid}`);
+		setCopySuccess('Copied!');
+		setTimeout(() => {
+			setCopySuccess('Copy');
+		}, 5000);
+	};
+
+	return (
+		<div id={styles.linkCard_container}>
+			<div>{url}</div>
+			<div>
+				<span className={styles.link_url}>{`https://rel.ink/${hashid}`}</span>
+				<button
+					className={
+						copySuccess === 'Copy'
+							? styles.primary_btn_color
+							: styles.tertiary_btn_color
+					}
+					onClick={copyToClipboard}>
+					{copySuccess}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default LinkCard;
