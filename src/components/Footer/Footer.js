b79238342632f7c26a312f55a1b1d875773edc69
tmoran18 <moran.tim01@gmail.com>
2020-09-15 19:10:28 +1000

    updated Link Card styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import styles from '../Footer/Footer.module.css'
+import React from "react";
+import styles from "../Footer/Footer.module.css";
 
 const Footer = () => {
-    return (
+  return (
+    <div id={styles.footer}>
+      <footer className={`${styles.footer} ${styles.container}`}>
         <div>
-            <footer className={`${styles.footer} ${styles.container}`} >
-                <div>
-                    <h2>Shortly</h2>
-                </div>
-                <div>
-                    <ul>
-                        <li className={styles.list_title}>Features</li>
-                        <li>Link Shortening</li>
-                        <li>Branded Links</li>
-                        <li>Analytics</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li className={styles.list_title}>Blog</li>
-                        <li>Developers</li>
-                        <li>Support</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li className={styles.list_title}>Company</li>
-                        <li>About</li>
-                        <li>Our Team</li>
-                        <li>Careers</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-                <div>
-                    <img src="../assets/icon-facebook.svg" alt=""/>
-                    <img src="./assets/icon-instagram.svg" alt=""/>
-                    <img src="./assets/icon-pinterest.svg" alt=""/>
-                    <img src="./assets/icon-twitter.svg" alt=""/>
-                </div>
-            </footer>
+          <h2>Shortly</h2>
         </div>
-    )
-}
+        <div>
+          <ul>
+            <li className={styles.list_title}>Features</li>
+            <li>Link Shortening</li>
+            <li>Branded Links</li>
+            <li>Analytics</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className={styles.list_title}>Blog</li>
+            <li>Developers</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className={styles.list_title}>Company</li>
+            <li>About</li>
+            <li>Our Team</li>
+            <li>Careers</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <img src="../assets/icon-facebook.svg" alt="" />
+          <img src="./assets/icon-instagram.svg" alt="" />
+          <img src="./assets/icon-pinterest.svg" alt="" />
+          <img src="./assets/icon-twitter.svg" alt="" />
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/LinkInput/LinkInput.js
+++ src/components/LinkInput/LinkInput.js
@@ -1,64 +1,59 @@
-import Axios from 'axios';
-import React, { useState, useEffect, useRef } from 'react';
-import styles from './LinkInput.module.css';
-import axios from 'axios';
-import LinkCard from '../LinkCard/LinkCard';
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./LinkInput.module.css";
+import axios from "axios";
+import LinkCard from "../LinkCard/LinkCard";
 
 const LinkInput = () => {
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [input, setInput] = useState('');
-	const loaded = useRef(false);
-	const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
 
-	//setItems(items => [...items, 'New Item']);
+  //setItems(items => [...items, 'New Item']);
 
-	const fetchLink = async () => {
-		const result = await axios.post(`https://rel.ink/api/links/`, {
-			url: input,
-		});
+  const fetchLink = async () => {
+    const result = await axios.post(`https://rel.ink/api/links/`, {
+      url: input,
+    });
 
-		setData((data) => [...data, result.data]);
-		setCount(() => count + 1);
-		setInput('');
-	};
+    setData((data) => [...data, result.data]);
+    setCount(() => count + 1);
+    setInput("");
+  };
 
-	useEffect(() => {
-		if (count === 0) {
-			// try get Local Storage
-			// if there is no data in localStorage, set state to empty Array
-			if (localStorage.getItem('data') === null) {
-				setData([]);
-			} else {
-				const retrievedData = JSON.parse(localStorage.getItem('data'));
-				setData(retrievedData);
-			}
-			console.log('component did mount');
-		} else {
-			// set Local Storage
-			localStorage.setItem('data', JSON.stringify(data));
-			console.log('component Did Update');
-		}
-	}, [count]);
+  useEffect(() => {
+    if (count === 0) {
+      // try get Local Storage
+      // if there is no data in localStorage, set state to empty Array
+      if (localStorage.getItem("data") === null) {
+        setData([]);
+      } else {
+        const retrievedData = JSON.parse(localStorage.getItem("data"));
+        setData(retrievedData);
+      }
+    } else {
+      // set Local Storage
+      localStorage.setItem("data", JSON.stringify(data));
+    }
+  }, [count]);
 
-	return (
-		<div>
-			<div id={styles.linkInput_container}>
-				<input
-					value={input}
-					onChange={(e) => setInput(e.target.value)}
-					type='text'
-					placeholder='Shorten a link here...'
-				/>
+  return (
+    <div className="container">
+      <div id={styles.linkInput_container}>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Shorten a link here..."
+        />
 
-				<button onClick={fetchLink}>Shorten It!</button>
-				<span>Please add a link (must have http or https)</span>
-			</div>
-			{data.map((link, index) => (
-				<LinkCard key={index} url={link.url} hashid={link.hashid} />
-			))}
-		</div>
-	);
+        <button onClick={fetchLink}>Shorten It!</button>
+        <span>Please add a link (must have http or https)</span>
+      </div>
+      {data.map((link, index) => (
+        <LinkCard key={index} url={link.url} hashid={link.hashid} />
+      ))}
+    </div>
+  );
 };
 
 export default LinkInput;
